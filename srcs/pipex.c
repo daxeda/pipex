ed381f7c1047e122d761716d5cc23022f518b693
daxesgh <118105444+daxesgh@users.noreply.github.com>
2023-08-23 03:30:11 +0200

    Update pipex.c

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,53 +6,52 @@
 /*   By: xeherzi <xeherzi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 11:07:21 by xeherzi           #+#    #+#             */
-/*   Updated: 2023/08/22 16:16:36 by xeherzi          ###   ########.fr       */
+/*   Updated: 2023/08/23 03:27:04 by xeherzi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	try_executing(char **executable_paths, char **command, char **environment)
+void	find_and_exec(char **cmd, char **envp)
 {
-	int i = 0;
+	char	**path;
 
-	while (executable_paths[i])
+	if (ft_strchr(*cmd, ' '))
+		cmd = ft_split(*cmd, ' ');
+	if (!cmd)
+		error_exit("malloc fail\n", 12);
+	if (access(*cmd, F_OK | X_OK) == 0)
 	{
-		if (access(executable_paths[i], F_OK | X_OK) == 0)
-			execve(executable_paths[i], command, environment);
-		i++;
+		execve(*cmd, cmd, envp);
+		error_exit("exec fail\n", 127);
 	}
+	path = find_path(envp, *cmd);
+	if (!path)
+		error_exit("malloc fail\n", 12);
+	while (path)
+	{
+		if (access(*path, F_OK | X_OK) == 0)
+		{
+			execve(*path, cmd, envp);
+			error_exit("exec fail", 127);
+		}
+		path++;
+	}
+	error_exit("No executable found", 2);
 }
 
-void	find_and_execute(char **command, char **environment)
+	static pid_t	safely_fork(void)
 {
-	char	**executable_paths;
-	int		i = 0;
+	pid_t	child_pid;
 
-	while (command[i])
-		i++;
-
-	if (i > 1)
-		command = ft_split(*command, ' ');
-
-	if (!command)
-		exit_with_error("Memory allocation failed", 12);
-
-	if (access(*command, F_OK | X_OK) == 0)
-		execve(*command, command, environment);
-	else
-	{
-		executable_paths = find_executable_paths(environment, *command);
-		if (!executable_paths)
-			exit_with_error("Executable paths not found", 12);
-
-		try_executing(executable_paths, command, environment);
-		exit_with_error("No executable found", 2);
-	}
+	child_pid = fork();
+	if (child_pid == -1)
+		error_exit("fork failed\n", 2);
+	return (child_pid);
 }
 
 
-void	baby_1(char **argv, char **envp, int *pipe_fd)
+void	child_1(char **argv, char **envp, int *pipe_fd)
 {
 	int	file_fd;
 
@@ -67,7 +66,7 @@ void	baby_1(char **argv, char **envp, int *pipe_fd)
 	find_and_exec(&argv[2], envp);
 }
 
-void	baby_2(char **argv, char **envp, int *pipe_fd)
+void	child_2(char **argv, char **envp, int *pipe_fd)
 {
 	int	file_fd;
 
@@ -82,38 +81,28 @@ void	baby_2(char **argv, char **envp, int *pipe_fd)
 	find_and_exec(&argv[3], envp);
 }
 
-static pid_t	safe_fork(void)
-{
-	pid_t	pid;
-
-	pid = fork();
-	if (pid == -1)
-		error_exit("fork failed\n", 2);
-	return (pid);
-}
-
 int	main(int argc, char **argv, char **envp)
 {
 	int		pipe_fd[2];
 	pid_t	pid_1;
 	pid_t	pid_2;
-	int		stat_loc;
+	int		status;
 
 	if (argc != 5)
-		error_exit("wrong nr of arguments\n", 1);
+		error_exit("invalid nbr of arguments\n", 1);
 	if (pipe(pipe_fd) == -1)
-		error_exit("pipe failed\n", 2);
-	pid_1 = safe_fork();
+		error_exit("pipe creation failed\n", 2);
+	pid_1 = safely_fork();
 	if (pid_1 == 0)
-		baby_1(argv, envp, pipe_fd);
-	pid_2 = safe_fork();
+		child_1(argv, envp, pipe_fd);
+	pid_2 = safely_fork();
 	if (pid_2 == 0)
-		baby_2(argv, envp, pipe_fd);
+		child_2(argv, envp, pipe_fd);
 	close(pipe_fd[READ]);
 	close(pipe_fd[WRITE]);
-	waitpid(pid_1, &stat_loc, 0);
-	waitpid(pid_2, &stat_loc, 0);
-	if (WIFEXITED(stat_loc))
-		exit(WEXITSTATUS(stat_loc));
+	waitpid(pid_1, &status, 0);
+	waitpid(pid_2, &status, 0);
+	if (WIFEXITED(status))
+		exit(WEXITSTATUS(status));
 	return (0);
 }
